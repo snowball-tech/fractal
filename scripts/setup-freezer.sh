1d8a926e8e404ad8f7b6d0d86b98849fd9c79e5b
Clément PRÉVOT <clement@snowball.xyz>
2023-06-19 18:03:28 +0200

    ci: fix the exposure of the machine token

--- scripts/setup-freezer.sh
+++ scripts/setup-freezer.sh
@@ -11,6 +11,4 @@ if [ -n "$CI" ]; then
   echo -n "$(info "Updating Git config to enforce HTTPS to connect to Freezer... ")"
   git config --global url."https://${SNOWBALL_BOT_TOKEN}@github.com/snowball-tech/freezer.git".insteadOf "git@github.com:snowball-tech/freezer.git"
   bold_success "DONE"
-
-  git config --global --list
 fi
