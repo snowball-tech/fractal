0418affb4abb7ff5338025345a0bb3167a1c289b
Clément PRÉVOT <clement@snowball.xyz>
2023-06-19 17:58:34 +0200

    ci: try to debug the copy of fonts

--- scripts/setup-freezer.sh
+++ scripts/setup-freezer.sh
@@ -11,4 +11,6 @@ if [ -n "$CI" ]; then
   echo -n "$(info "Updating Git config to enforce HTTPS to connect to Freezer... ")"
   git config --global url."https://${SNOWBALL_BOT_TOKEN}@github.com/snowball-tech/freezer.git".insteadOf "git@github.com:snowball-tech/freezer.git"
   bold_success "DONE"
+
+  git config --global --list
 fi
