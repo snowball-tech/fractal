77d2bf14fcf90960fe54f92df4d0272072de4fa8
Clément PRÉVOT <clement@snowball.xyz>
2023-06-19 18:48:21 +0200

    ci: rename the env var

--- scripts/setup-freezer.sh
+++ scripts/setup-freezer.sh
@@ -9,6 +9,6 @@ source "$(dirname "$0")/colors.sh"
 
 if [ -n "$CI" ]; then
   echo -n "$(info "Updating Git config to enforce HTTPS to connect to Freezer... ")"
-  git config --global url."https://${SNOWBALL_BOT_TOKEN}@github.com/snowball-tech/freezer.git".insteadOf "git@github.com:snowball-tech/freezer.git"
+  git config --global url."https://${FREEZER_TOKEN}@github.com/snowball-tech/freezer.git".insteadOf "git@github.com:snowball-tech/freezer.git"
   bold_success "DONE"
 fi
