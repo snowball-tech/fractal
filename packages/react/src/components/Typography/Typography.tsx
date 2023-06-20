3c8935e57f776ae17f4fbbcb44787e35c5dd3459
Clément PRÉVOT <clement@snowball.xyz>
2023-06-20 13:57:10 +0200

    fix(typography): add responsive size to heading fonts

--- packages/react/src/components/Typography/Typography.tsx
+++ packages/react/src/components/Typography/Typography.tsx
@@ -1,15 +1,32 @@
+import * as DesignTokens from '@snowball-tech/design-tokens/dist/web/react/design-tokens'
 import { styled } from 'styled-components'
+
+import { Breakpoints } from '@/constants'
+import type { Breakpoint } from '@/types'
+import { getBreakpointCssVarWithFallback } from '@/utils'
 
 import { VARIANTS_MAPPING, Variants } from './Typography.constants'
 import type { TypographyProps } from './Typography.types'
 
 const DEFAULT_VARIANT = Variants.Body1
 
+function getFontSize(variant: Variants, breakpoint: Breakpoint): string {
+  return getBreakpointCssVarWithFallback(
+    `--typography-${variant}-font-size`,
+    breakpoint,
+  )
+}
+
+function getLineHeight(variant: Variants, breakpoint: Breakpoint): string {
+  return getBreakpointCssVarWithFallback(
+    `--typography-${variant}-line-height`,
+    breakpoint,
+  )
+}
+
 const StyledTypography = styled.div<TypographyProps>`
   font-family: ${({ variant = DEFAULT_VARIANT }) =>
     `var(--typography-${variant}-font-family)`};
-  font-size: ${({ variant = DEFAULT_VARIANT }) =>
-    `var(--typography-${variant}-font-size)`};
   font-weight: ${({ variant = DEFAULT_VARIANT }) =>
     `var(--typography-${variant}-font-weight)`};
 
@@ -20,6 +37,48 @@ const StyledTypography = styled.div<TypographyProps>`
 
   text-decoration: ${({ variant = DEFAULT_VARIANT }) =>
     `var(--typography-${variant}-text-decoration)`};
+
+  @media ${DesignTokens.MediaQueryFromXs} {
+    font-size: ${({ variant = DEFAULT_VARIANT }) =>
+      getFontSize(variant, Breakpoints.xs)};
+    line-height: ${({ variant = DEFAULT_VARIANT }) =>
+      getLineHeight(variant, Breakpoints.xs)};
+  }
+
+  @media ${DesignTokens.MediaQueryFromSm} {
+    font-size: ${({ variant = DEFAULT_VARIANT }) =>
+      getFontSize(variant, Breakpoints.sm)};
+    line-height: ${({ variant = DEFAULT_VARIANT }) =>
+      getLineHeight(variant, Breakpoints.sm)};
+  }
+
+  @media ${DesignTokens.MediaQueryFromMd} {
+    font-size: ${({ variant = DEFAULT_VARIANT }) =>
+      getFontSize(variant, Breakpoints.md)};
+    line-height: ${({ variant = DEFAULT_VARIANT }) =>
+      getLineHeight(variant, Breakpoints.md)};
+  }
+
+  @media ${DesignTokens.MediaQueryFromLg} {
+    font-size: ${({ variant = DEFAULT_VARIANT }) =>
+      getFontSize(variant, Breakpoints.lg)};
+    line-height: ${({ variant = DEFAULT_VARIANT }) =>
+      getLineHeight(variant, Breakpoints.lg)};
+  }
+
+  @media ${DesignTokens.MediaQueryFromXl} {
+    font-size: ${({ variant = DEFAULT_VARIANT }) =>
+      getFontSize(variant, Breakpoints.xl)};
+    line-height: ${({ variant = DEFAULT_VARIANT }) =>
+      getLineHeight(variant, Breakpoints.xl)};
+  }
+
+  @media ${DesignTokens.MediaQueryFromXxl} {
+    font-size: ${({ variant = DEFAULT_VARIANT }) =>
+      getFontSize(variant, Breakpoints.xxl)};
+    line-height: ${({ variant = DEFAULT_VARIANT }) =>
+      getLineHeight(variant, Breakpoints.xxl)};
+  }
 `
 
 /**
