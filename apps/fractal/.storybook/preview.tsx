6d21ea194a9d9cbb78f5d38717687964f6c3dea8
Clément PRÉVOT <clement@snowball.xyz>
2023-06-20 12:58:51 +0200

    chore(fractal): remove useless storybook addon

--- apps/fractal/.storybook/preview.tsx
+++ apps/fractal/.storybook/preview.tsx
@@ -2,29 +2,14 @@ import {
   ColorBaseBlack,
   ColorBaseWhite,
 } from '@snowball-tech/design-tokens/dist/web/react/design-tokens'
-import { withThemeFromJSXProvider } from '@storybook/addon-styling'
 import type { Preview } from '@storybook/react'
-import { createGlobalStyle } from 'styled-components'
 
 import DocumentationTemplate from './DocumentationTemplate.mdx'
 import fractalTheme from './theme'
 
 import '../global.css'
 
-const GlobalStyles = createGlobalStyle`
-  body {
-    font-family: var(--font-family-normal);
-    background-color: var(--color-brand-body);
-  }
-`
-
 const preview = {
-  decorators: [
-    withThemeFromJSXProvider({
-      GlobalStyles,
-    }),
-  ],
-
   parameters: {
     actions: { argTypesRegex: '^on[A-Z].*' },
 
--- packages/react/.storybook/preview.tsx
+++ packages/react/.storybook/preview.tsx
@@ -1,3 +1,35 @@
-import preview from '@snowball-tech/fractal/.storybook/preview'
+import {
+  ColorBaseBlack,
+  ColorBaseWhite,
+} from '@snowball-tech/design-tokens/dist/web/react/design-tokens'
+import fractalTheme from '@snowball-tech/fractal/.storybook/theme'
+import type { Preview } from '@storybook/react'
+
+import DocumentationTemplate from './DocumentationTemplate.mdx'
+
+import '@snowball-tech/fractal/global.css'
+
+const preview = {
+  parameters: {
+    actions: { argTypesRegex: '^on[A-Z].*' },
+
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/,
+      },
+    },
+
+    docs: {
+      page: DocumentationTemplate,
+      theme: {
+        ...fractalTheme,
+
+        textColor: ColorBaseBlack,
+        textInverseColor: ColorBaseWhite,
+      },
+    },
+  },
+} as Preview
 
 export default preview
