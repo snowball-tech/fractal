3b8ca134c9adc6b9e833d11e7215a51b38d05883
Clément PRÉVOT <clement@snowball.xyz>
2023-06-30 10:11:01 +0200

    fix(design-token): rename some tokens and update body value

--- packages/design-tokens/tokens/color/brand.js
+++ packages/design-tokens/tokens/color/brand.js
@@ -7,8 +7,7 @@ module.exports = {
       secondary: { comment: 'Tone 0', value: '#000000' },
 
       // eslint-disable-next-line sort-keys, sort-keys/sort-keys-fix, perfectionist/sort-objects
-      body: { comment: 'Tone 90', value: '#FFFBEE' },
-      separator: { comment: 'Tone 90', value: '#E2E2E2' },
+      bodyLight: { comment: 'Tone 90', value: '#FFFBF4' },
     },
   },
 }
