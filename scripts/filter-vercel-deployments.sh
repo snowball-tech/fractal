595f5bbb228c16d66294295e3a903a652a31a544
Clément PRÉVOT <clement@snowball.xyz>
2023-07-01 10:01:53 +0200

    fix: fix the execution of the filter vercel deployments script

--- scripts/filter-vercel-deployments.sh
+++ scripts/filter-vercel-deployments.sh
@@ -1,13 +1,16 @@
-#!/usr/bin/env bash
+#!/usr/bin/bash
+
+echo "Checking if Vercel deployment should be skipped..."
 
 # If VERCEL_GIT_COMMIT_REF env var starts with "renovate/" then exit with 0 to
 # block the Vercel deployment.
 # Otherwise exit with 1 to carry on with the deployment.
 
 if [[ "${VERCEL_GIT_COMMIT_REF}" == renovate/* ]]; then
-  echo "Renovate branch detected, skipping build"
+  echo "Renovate branch detected! Skipping deployment!"
   exit 0
-else
-  echo "Standard branch, continuing build"
-  exit 1
 fi
+
+echo "Continuing with deployment!"
+
+exit 1
