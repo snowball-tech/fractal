420a7b994507d2f59a52c0d407c084d64180587a
Clément PRÉVOT <clement@snowball.xyz>
2023-07-03 17:06:40 +0200

    feat(design-tokens): add grey colors

--- packages/design-tokens/tokens/color/base.js
+++ packages/design-tokens/tokens/color/base.js
@@ -1,8 +1,12 @@
 module.exports = {
   color: {
     base: {
-      black: { comment: 'Tone 0', value: '#000000' },
-      white: { comment: 'Tone 100', value: '#FFFFFF' },
+      black: { comment: 'Black Tone 0', value: '#000000' },
+      grey: {
+        50: { comment: 'Grey Tone 50', value: '#B7B1A6' },
+        70: { comment: 'Grey Tone 70', value: '#DDD9D4' },
+      },
+      white: { comment: 'White Tone 100', value: '#FFFFFF' },
 
       // eslint-disable-next-line sort-keys, sort-keys/sort-keys-fix, perfectionist/sort-objects
       separator: { comment: 'Tone 90', value: '#E2E2E2' },
