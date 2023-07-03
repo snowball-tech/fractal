2d72a9ad0a02896b5349caa02b6bb02da60d7424
Clément PRÉVOT <clement@snowball.xyz>
2023-07-03 17:27:00 +0200

    deps: fix dependencies

---
+++ packages/design-tokens/tokens/size/button.js
@@ -0,0 +1,25 @@
+module.exports = {
+  size: {
+    button: {
+      padding: {
+        horizontal: {
+          comment: 'Horizontal padding for any button',
+
+          value: '{size.spacing.3.value}',
+        },
+        vertical: {
+          comment: 'Vertical padding for any button',
+
+          value: '{size.spacing.1.value}',
+        },
+      },
+
+      // eslint-disable-next-line sort-keys, sort-keys/sort-keys-fix, perfectionist/sort-objects
+      gap: {
+        comment: 'Gap between text and icon',
+
+        value: '{size.spacing.2.value}',
+      },
+    },
+  },
+}
