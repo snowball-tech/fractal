0aa09d2c49d77813dbc3c7d99885871b11515388
Clément PRÉVOT <clement@snowball.xyz>
2023-07-03 17:06:06 +0200

    docs(design-tokens): improve comment of decorative colors

--- packages/design-tokens/tokens/color/decorative.js
+++ packages/design-tokens/tokens/color/decorative.js
@@ -5,28 +5,28 @@ module.exports = {
 
       // eslint-disable-next-line sort-keys, sort-keys/sort-keys-fix, perfectionist/sort-objects
       blue: {
-        50: { comment: 'Tone 50', value: '#8AF1FF' },
-        70: { comment: 'Tone 70', value: '#B3F6FF' },
-        90: { comment: 'Tone 90', value: '#D6FAFF' },
+        50: { comment: 'Blue Tone 50', value: '#8AF1FF' },
+        70: { comment: 'Blue Tone 70', value: '#B3F6FF' },
+        90: { comment: 'Blue Tone 90', value: '#D6FAFF' },
       },
       green: {
-        50: { comment: 'Tone 50', value: '#76F7AE' },
-        70: { comment: 'Tone 70', value: '#ADFACE' },
-        90: { comment: 'Tone 90', value: '#CFFCE3' },
+        50: { comment: 'Green Tone 50', value: '#76F7AE' },
+        70: { comment: 'Green Tone 70', value: '#ADFACE' },
+        90: { comment: 'Green Tone 90', value: '#CFFCE3' },
       },
       pink: {
-        70: { comment: 'Tone 70', value: '#FFC6E7' },
-        90: { comment: 'Tone 90', value: '#FFE2F3' },
+        70: { comment: 'Pink Tone 70', value: '#FFC6E7' },
+        90: { comment: 'Pink Tone 90', value: '#FFE2F3' },
       },
       purple: {
-        50: { comment: 'Tone 50', value: '#B37DFF' },
-        70: { comment: 'Tone 70', value: '#D9BEFF' },
-        90: { comment: 'Tone 90', value: '#F0E5FF' },
+        50: { comment: 'Purple Tone 50', value: '#B37DFF' },
+        70: { comment: 'Purple Tone 70', value: '#D9BEFF' },
+        90: { comment: 'Purple Tone 90', value: '#F0E5FF' },
       },
       yellow: {
-        50: { comment: 'Tone 50', value: '#FFE959' },
-        70: { comment: 'Tone 70', value: '#FFF29B' },
-        90: { comment: 'Tone 90', value: '#FFF8C5' },
+        50: { comment: 'Yellow Tone 50', value: '#FFE959' },
+        70: { comment: 'Yellow Tone 70', value: '#FFF29B' },
+        90: { comment: 'Yellow Tone 90', value: '#FFF8C5' },
       },
     },
   },
