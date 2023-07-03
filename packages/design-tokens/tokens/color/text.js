5498390c9eda446bb6b4a28186068baadc8f2dac
Clément PRÉVOT <clement@snowball.xyz>
2023-07-03 16:59:03 +0200

    feat(design-token): add text color tokens

--- packages/design-tokens/tokens/color/text.js
+++ packages/design-tokens/tokens/color/text.js
@@ -1,5 +1,24 @@
 module.exports = {
   color: {
-    text: {},
+    text: {
+      dark: {
+        comment: 'Color to use for texts over light background.',
+
+        value: '{color.base.black.value}',
+      },
+
+      light: {
+        comment: 'Color to use for texts over dark background.',
+
+        value: '{color.base.white.value}',
+      },
+
+      // eslint-disable-next-line sort-keys, sort-keys/sort-keys-fix, perfectionist/sort-objects
+      disabled: {
+        comment: 'Color to use for texts of disabled elements',
+
+        value: '#B7B1A6',
+      },
+    },
   },
 }
