d6cad5cee738858e61bc7d1c49275666396b3fde
Clément PRÉVOT <clement@snowball.xyz>
2023-07-03 17:55:08 +0200

    docs(design-tokens): improve the comments of the shadow tokens

--- packages/design-tokens/tokens/shadow.js
+++ packages/design-tokens/tokens/shadow.js
@@ -5,17 +5,18 @@ module.exports = {
 
       value: 'none',
     },
+
     // eslint-disable-next-line sort-keys, sort-keys/sort-keys-fix, perfectionist/sort-objects
     brutal: {
       1: {
         comment:
-          'A more subtle shadow, always combined with radius or rounded shapes and with border-1.',
+          'A subtle shadow, always combined with radius or rounded shapes and with border-1.',
 
         value: '-2px 2px 0 0 rgba(0, 0, 0, 1)',
       },
       2: {
         comment:
-          'The most proeminent shadow, always combined with radius or rounded shapes and with border-1 or border-2.',
+          'A proeminent shadow, always combined with radius or rounded shapes and with border-1 or border-2.',
 
         value: '-2px 4px 0 0 rgba(0, 0, 0, 1)',
       },
