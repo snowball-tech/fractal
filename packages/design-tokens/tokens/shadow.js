ec4b544932cbb84316195e1581c4b9d3c02e93d7
Clément PRÉVOT <clement@snowball.xyz>
2023-07-03 16:58:11 +0200

    feat(design-tokens): add a new shadow token

--- packages/design-tokens/tokens/shadow.js
+++ packages/design-tokens/tokens/shadow.js
@@ -1,8 +1,14 @@
 module.exports = {
   shadow: {
-    brutal: {
+    'brutal-1': {
       comment:
-        'The only shadow, always combined with radius or rounded shapes and with border-1 or border-2.',
+        'A more subtle shadow, always combined with radius or rounded shapes and with border-1.',
+
+      value: '-2px 2px 0 0 rgba(0, 0, 0, 1)',
+    },
+    'brutal-2': {
+      comment:
+        'The most proeminent shadow, always combined with radius or rounded shapes and with border-1 or border-2.',
 
       value: '-2px 4px 0 0 rgba(0, 0, 0, 1)',
     },
