acceb4e684a8fdfccea30d605d1dde1cfbf36c91
Clément PRÉVOT <clement@snowball.xyz>
2023-07-03 17:05:06 +0200

    break(design-token): move body out of brand and to background

--- packages/design-tokens/tokens/color/background.js
+++ packages/design-tokens/tokens/color/background.js
@@ -1,5 +1,10 @@
 module.exports = {
   color: {
-    background: {},
+    background: {
+      // eslint-disable-next-line sort-keys, sort-keys/sort-keys-fix, perfectionist/sort-objects
+      body: {
+        light: { comment: 'Yellowish White Tone 90', value: '#FFFBF4' },
+      },
+    },
   },
 }
--- packages/design-tokens/tokens/color/brand.js
+++ packages/design-tokens/tokens/color/brand.js
@@ -5,11 +5,6 @@ module.exports = {
 
       primary: { comment: 'Tone 50', value: '#FF8ACD' },
       secondary: { comment: 'Tone 0', value: '#000000' },
-
-      // eslint-disable-next-line sort-keys, sort-keys/sort-keys-fix, perfectionist/sort-objects
-      body: {
-        light: { comment: 'Tone 90', value: '#FFFBF4' },
-      },
     },
   },
 }
