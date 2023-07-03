7fc1f3aeffc00108f18ebbde1311d02d985b6e47
Clément PRÉVOT <clement@snowball.xyz>
2023-07-03 17:25:26 +0200

    feat(design-tokens): add tokens for buttons

--- packages/design-tokens/tokens/color/text.js
+++ packages/design-tokens/tokens/color/text.js
@@ -2,22 +2,105 @@ module.exports = {
   color: {
     text: {
       dark: {
-        comment: 'Color to use for texts over light background.',
+        comment: 'Text color over light background.',
 
         value: '{color.base.black.value}',
       },
 
       light: {
-        comment: 'Color to use for texts over dark background.',
+        comment: 'Text color over dark background.',
 
         value: '{color.base.white.value}',
       },
 
       // eslint-disable-next-line sort-keys, sort-keys/sort-keys-fix, perfectionist/sort-objects
       disabled: {
-        comment: 'Color to use for texts of disabled elements',
+        comment: 'Text color of most disabled elements',
 
         value: '#B7B1A6',
+      },
+
+      // eslint-disable-next-line sort-keys, sort-keys/sort-keys-fix, perfectionist/sort-objects
+      button: {
+        display: {
+          base: {
+            comment: 'Text color to use on display elements (e.g. buttons).',
+
+            value: '{color.base.black.value}',
+          },
+          hover: {
+            comment:
+              'Background color to use when hovering/focusing display elements (e.g. buttons).',
+
+            value: '{color.text.button.display.base.value}',
+          },
+          // eslint-disable-next-line sort-keys, sort-keys/sort-keys-fix, perfectionist/sort-objects
+          active: {
+            comment:
+              'Background color to use when clicking/activating a display elements (e.g. buttons).',
+
+            value: '{color.text.button.display.base.value}',
+          },
+          disabled: {
+            comment:
+              'Background color to use on disabled display elements (e.g. buttons).',
+
+            value: '{color.base.grey.50.value}',
+          },
+        },
+        primary: {
+          base: {
+            comment: 'Text color to use on primary elements (e.g. buttons).',
+
+            value: '{color.base.white.value}',
+          },
+          hover: {
+            comment:
+              'Background color to use when hovering/focusing primary elements (e.g. buttons).',
+
+            value: '{color.base.black.value}',
+          },
+          // eslint-disable-next-line sort-keys, sort-keys/sort-keys-fix, perfectionist/sort-objects
+          active: {
+            comment:
+              'Background color to use when clicking/activating a primary elements (e.g. buttons).',
+
+            value: '{color.text.button.primary.hover.value}',
+          },
+          disabled: {
+            comment:
+              'Background color to use on disabled primary elements (e.g. buttons).',
+
+            value: '{color.text.button.primary.base.value}',
+          },
+        },
+        secondary: {
+          base: {
+            comment:
+              'Background color to use on secondary elements (e.g. buttons).',
+
+            value: '{color.base.black.value}',
+          },
+          hover: {
+            comment:
+              'Background color to use when hovering/focusing secondary elements (e.g. buttons).',
+
+            value: '{color.text.button.secondary.base.value}',
+          },
+          // eslint-disable-next-line sort-keys, sort-keys/sort-keys-fix, perfectionist/sort-objects
+          active: {
+            comment:
+              'Background color to use when clicking/activating a secondary elements (e.g. buttons).',
+
+            value: '{color.base.white.value}',
+          },
+          disabled: {
+            comment:
+              'Background color to use on disabled secondary elements (e.g. buttons).',
+
+            value: '{color.base.grey.50.value}',
+          },
+        },
       },
     },
   },
--- packages/design-tokens/tokens/shadow.js
+++ packages/design-tokens/tokens/shadow.js
@@ -1,16 +1,102 @@
 module.exports = {
   shadow: {
-    'brutal-1': {
-      comment:
-        'A more subtle shadow, always combined with radius or rounded shapes and with border-1.',
+    none: {
+      comment: 'Disable shadow.',
 
-      value: '-2px 2px 0 0 rgba(0, 0, 0, 1)',
+      value: 'none',
     },
-    'brutal-2': {
-      comment:
-        'The most proeminent shadow, always combined with radius or rounded shapes and with border-1 or border-2.',
+    // eslint-disable-next-line sort-keys, sort-keys/sort-keys-fix, perfectionist/sort-objects
+    brutal: {
+      1: {
+        comment:
+          'A more subtle shadow, always combined with radius or rounded shapes and with border-1.',
 
-      value: '-2px 4px 0 0 rgba(0, 0, 0, 1)',
+        value: '-2px 2px 0 0 rgba(0, 0, 0, 1)',
+      },
+      2: {
+        comment:
+          'The most proeminent shadow, always combined with radius or rounded shapes and with border-1 or border-2.',
+
+        value: '-2px 4px 0 0 rgba(0, 0, 0, 1)',
+      },
+    },
+
+    button: {
+      display: {
+        base: {
+          comment: 'Shadow for display elements (e.g. buttons).',
+
+          value: '{shadow.brutal.1.value}',
+        },
+        hover: {
+          comment:
+            'Shadow when hovering/focusing display elements (e.g. buttons).',
+
+          value: '{shadow.brutal.2.value}',
+        },
+        // eslint-disable-next-line sort-keys, sort-keys/sort-keys-fix, perfectionist/sort-objects
+        active: {
+          comment:
+            'Shadow when clicking/activating display elements (e.g. buttons).',
+
+          value: '{shadow.none.value}',
+        },
+        disabled: {
+          comment: 'Shadow for disabled display elements (e.g. buttons).',
+
+          value: '{shadow.none.value}',
+        },
+      },
+      primary: {
+        base: {
+          comment: 'Shadow for primary elements (e.g. buttons).',
+
+          value: '{shadow.none.value}',
+        },
+        hover: {
+          comment:
+            'Shadow when hovering/focusing primary elements (e.g. buttons).',
+
+          value: '{shadow.button.primary.base.value}',
+        },
+        // eslint-disable-next-line sort-keys, sort-keys/sort-keys-fix, perfectionist/sort-objects
+        active: {
+          comment:
+            'Shadow when clicking/activating primary elements (e.g. buttons).',
+
+          value: '{shadow.button.primary.base.value}',
+        },
+        disabled: {
+          comment: 'Shadow for disabled primary elements (e.g. buttons).',
+
+          value: '{shadow.button.primary.base.value}',
+        },
+      },
+      secondary: {
+        base: {
+          comment: 'Shadow for secondary elements (e.g. buttons).',
+
+          value: '{shadow.none.value}',
+        },
+        hover: {
+          comment:
+            'Shadow when hovering/focusing secondary elements (e.g. buttons).',
+
+          value: '{shadow.button.secondary.base.value}',
+        },
+        // eslint-disable-next-line sort-keys, sort-keys/sort-keys-fix, perfectionist/sort-objects
+        active: {
+          comment:
+            'Shadow when clicking/activating secondary elements (e.g. buttons).',
+
+          value: '{shadow.button.secondary.base.value}',
+        },
+        disabled: {
+          comment: 'Shadow for disabled secondary elements (e.g. buttons).',
+
+          value: '{shadow.button.secondary.base.value}',
+        },
+      },
     },
   },
 }
