84010fbe4bb3a6ce72c71a51272e0b7b7587a3c8
Clément PRÉVOT <clement@snowball.xyz>
2023-07-04 17:51:32 +0200

    style(button): improve the style of button and the story

--- packages/fractal/src/components/Button/Button.types.ts
+++ packages/fractal/src/components/Button/Button.types.ts
@@ -5,7 +5,7 @@ import { Variants } from './Button.constants'
 export interface ButtonProps extends HTMLAttributes<HTMLButtonElement> {
   /** When `true`, prevents the user from interacting with the button. */
   disabled?: boolean
-  /* The icon to display in the button (if any). */
+  /** The icon to display in the button (if any). */
   icon?: ReactNode
   /** The position of the icon in the button. */
   iconPosition?: 'left' | 'right'
