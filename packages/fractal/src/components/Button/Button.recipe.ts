1e67ef21543b392279f227824ac0536896294427
Clément PRÉVOT <clement@snowball.xyz>
2023-07-04 16:44:19 +0200

    fix(button): improve hover/focus states and update deps

--- packages/fractal/src/components/Button/Button.recipe.ts
+++ packages/fractal/src/components/Button/Button.recipe.ts
@@ -3,23 +3,84 @@ import type { SystemStyleObject } from '@snowball-tech/fractal-panda/types'
 
 import { DEFAULT_VARIANT, Variants } from './Button.constants'
 
+const hoverFocus: { [K in Variants]?: SystemStyleObject } = {}
+
+const variantsObject = Object.values(Variants).reduce(
+  (variants, variantName) => {
+    hoverFocus[variantName] = {
+      '&:not(:is(:disabled, [disabled], [data-disabled]))': {
+        backgroundColor: `var(--color-background-button-${variantName}-hover)`,
+        border: `var(--border-button-${variantName}-hover)`,
+        color: `var(--color-text-button-${variantName}-hover)`,
+        shadow: `var(--shadow-button-${variantName}-hover)`,
+      },
+    }
+
+    return {
+      ...variants,
+      [variantName]: {
+        '&:not(:is(:active, [data-active]))': {
+          _focus: hoverFocus[variantName],
+          _hover: hoverFocus[variantName],
+        },
+
+        _active: {
+          '&:not(:is(:disabled, [disabled], [data-disabled]))': {
+            backgroundColor: `var(--color-background-button-${variantName}-active)`,
+            border: `var(--border-button-${variantName}-active)`,
+            color: `var(--color-text-button-${variantName}-active)`,
+            shadow: `var(--shadow-button-${variantName}-active)`,
+          },
+        },
+
+        _disabled: {
+          backgroundColor: `var(--color-background-button-${variantName}-disabled)`,
+          border: `var(--border-button-${variantName}-disabled)`,
+          color: `var(--color-text-button-${variantName}-disabled)`,
+          shadow: `var(--shadow-button-${variantName}-disabled)`,
+        },
+
+        backgroundColor: `var(--color-background-button-${variantName}-base)`,
+        border: `var(--border-button-${variantName}-base)`,
+        color: `var(--color-text-button-${variantName}-base)`,
+        shadow: `var(--shadow-button-${variantName}-base)`,
+      },
+    }
+  },
+  {} as Record<Variants, SystemStyleObject>,
+)
+
 export const button: ReturnType<typeof defineRecipe> = defineRecipe({
   description: 'Button',
   name: 'button',
 
   // eslint-disable-next-line sort-keys, sort-keys/sort-keys-fix, perfectionist/sort-objects
   base: {
+    '& > *': {
+      alignItems: 'center',
+      display: 'flex',
+      justifyContent: 'center',
+      margin: 0,
+    },
+
     _disabled: {
       cursor: 'not-allowed',
     },
 
+    alignItems: 'center',
     all: 'unset',
     borderRadius: 'var(--size-radius-rounded)',
     boxSizing: 'border-box',
     cursor: 'pointer',
+    display: 'flex',
+    height: '48px',
+    justifyContent: 'center',
+    maxHeight: '48px',
     maxWidth: 'fit-content',
+    outline: 'none',
     px: 'var(--size-button-padding-horizontal)',
     py: 'var(--size-button-padding-vertical)',
+    textAlign: 'center',
   },
 
   defaultVariants: {
@@ -27,49 +88,86 @@ export const button: ReturnType<typeof defineRecipe> = defineRecipe({
   },
 
   variants: {
-    variant: Object.values(Variants).reduce(
-      (variants, variantName) => ({
-        ...variants,
-        [variantName]: {
-          '&:not(:is(:active, [data-active]))': {
-            _focus: {
-              backgroundColor: `var(--color-background-button-${variantName}-hover)`,
-              border: `var(--border-button-${variantName}-hover)`,
-              color: `var(--color-text-button-${variantName}-hover)`,
-              shadow: `var(--shadow-button-${variantName}-hover)`,
-            },
+    variant: {
+      ...variantsObject,
 
-            _hover: {
-              '&:not(:is(:disabled, [disabled], [data-disabled]))': {
-                backgroundColor: `var(--color-background-button-${variantName}-hover)`,
-                border: `var(--border-button-${variantName}-hover)`,
-                color: `var(--color-text-button-${variantName}-hover)`,
-                shadow: `var(--shadow-button-${variantName}-hover)`,
-              },
-            },
+      [Variants.Display]: {
+        ...variantsObject[Variants.Display],
+
+        _active: {
+          // eslint-disable-next-line no-underscore-dangle
+          ...variantsObject[Variants.Display]?._active,
+          '&:not(:is(:disabled, [disabled], [data-disabled]))': {
+            // eslint-disable-next-line no-underscore-dangle
+            ...variantsObject[Variants.Display]?._active?.[
+              '&:not(:is(:disabled, [disabled], [data-disabled]))'
+            ],
+
+            transform: 'translate(-2px, 4px)',
           },
-
-          _active: {
-            backgroundColor: `var(--color-background-button-${variantName}-active)`,
-            border: `var(--border-button-${variantName}-active)`,
-            color: `var(--color-text-button-${variantName}-active)`,
-            shadow: `var(--shadow-button-${variantName}-active)`,
-          },
-
-          _disabled: {
-            backgroundColor: `var(--color-background-button-${variantName}-disabled)`,
-            border: `var(--border-button-${variantName}-disabled)`,
-            color: `var(--color-text-button-${variantName}-disabled)`,
-            shadow: `var(--shadow-button-${variantName}-disabled)`,
-          },
-
-          backgroundColor: `var(--color-background-button-${variantName}-base)`,
-          border: `var(--border-button-${variantName}-base)`,
-          color: `var(--color-text-button-${variantName}-base)`,
-          shadow: `var(--shadow-button-${variantName}-base)`,
         },
-      }),
-      {} as Record<Variants, SystemStyleObject>,
-    ),
+
+        _focus: {
+          ...hoverFocus[Variants.Display],
+          '&:not(:is(:disabled, [disabled], [data-disabled]))': {
+            ...hoverFocus[Variants.Display]?.[
+              '&:not(:is(:disabled, [disabled], [data-disabled]))'
+            ],
+
+            transform: 'translate(0, -2px)',
+          },
+        },
+
+        _hover: {
+          ...hoverFocus[Variants.Display],
+          '&:not(:is(:disabled, [disabled], [data-disabled]))': {
+            ...hoverFocus[Variants.Display]?.[
+              '&:not(:is(:disabled, [disabled], [data-disabled]))'
+            ],
+
+            transform: 'translate(0, -2px)',
+          },
+        },
+      },
+
+      [Variants.Secondary]: {
+        ...variantsObject[Variants.Secondary],
+
+        _active: {
+          // eslint-disable-next-line no-underscore-dangle
+          ...variantsObject[Variants.Secondary]?._active,
+          '&:not(:is(:disabled, [disabled], [data-disabled]))': {
+            // eslint-disable-next-line no-underscore-dangle
+            ...variantsObject[Variants.Secondary]?._active?.[
+              '&:not(:is(:disabled, [disabled], [data-disabled]))'
+            ],
+
+            margin: '0 -1px',
+          },
+        },
+
+        _focus: {
+          ...hoverFocus[Variants.Secondary],
+          '&:not(:is(:disabled, [disabled], [data-disabled]))': {
+            ...hoverFocus[Variants.Secondary]?.[
+              '&:not(:is(:disabled, [disabled], [data-disabled]))'
+            ],
+
+            margin: '0 -1px',
+          },
+        },
+
+        _hover: {
+          ...hoverFocus[Variants.Secondary],
+          '&:not(:is(:disabled, [disabled], [data-disabled]))': {
+            ...hoverFocus[Variants.Secondary]?.[
+              '&:not(:is(:disabled, [disabled], [data-disabled]))'
+            ],
+
+            margin: '0 -1px',
+          },
+        },
+      },
+    },
   },
 })
--- packages/fractal/src/components/Button/Button.types.ts
+++ packages/fractal/src/components/Button/Button.types.ts
@@ -3,10 +3,14 @@ import type { HTMLAttributes, MouseEvent, ReactNode } from 'react'
 import { Variants } from './Button.constants'
 
 export interface ButtonProps extends HTMLAttributes<HTMLButtonElement> {
-  /** The content of the button. */
-  children: ReactNode
   /** When `true`, prevents the user from interacting with the button. */
   disabled?: boolean
+  /* The icon to display in the button (if any). */
+  icon?: ReactNode
+  /** The position of the icon in the button. */
+  iconPosition?: 'left' | 'right'
+  /** The label of the button. */
+  label: string
   /** Event handler called when the button is clicked. */
   onClick?: (event: MouseEvent) => void
   /** The variant of the button  to use. */
