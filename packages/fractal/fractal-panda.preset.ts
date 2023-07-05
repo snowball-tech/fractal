9254fec917b3e578703db5994e2ec62344a15a5a
Clément PRÉVOT <clement@snowball.xyz>
2023-07-05 16:01:59 +0200

    fix(fractal): add missing preset file

---
+++ packages/fractal/fractal-panda.preset.ts
@@ -0,0 +1,46 @@
+import { definePreset } from '@pandacss/dev'
+import {
+  SizeBreakpointLg,
+  SizeBreakpointMd,
+  SizeBreakpointSm,
+  SizeBreakpointXl,
+  SizeBreakpointXs,
+  SizeBreakpointXxl,
+} from '@snowball-tech/design-tokens'
+
+import { Breakpoints } from '@/constants'
+import * as recipes from '@/recipes'
+
+const fractalPreset = definePreset({
+  /* eslint-disable sort-keys, sort-keys/sort-keys-fix, perfectionist/sort-objects */
+
+  // Don't use default theme from PandaCSS.
+  eject: true,
+  // Only import the base presets with useful resets and utilities.
+  presets: ['@pandacss/preset-base'],
+
+  theme: {
+    breakpoints: {
+      [Breakpoints.lg]: SizeBreakpointLg,
+      [Breakpoints.md]: SizeBreakpointMd,
+      [Breakpoints.sm]: SizeBreakpointSm,
+      [Breakpoints.xl]: SizeBreakpointXl,
+      [Breakpoints.xs]: SizeBreakpointXs,
+      [Breakpoints.xxl]: SizeBreakpointXxl,
+    },
+
+    extend: {
+      recipes: {
+        button: recipes.button,
+        checkboxIndicator: recipes.checkboxIndicator,
+        checkboxLabel: recipes.checkboxLabel,
+        checkboxRoot: recipes.checkboxRoot,
+        typography: recipes.typography,
+      },
+    },
+  },
+
+  /* eslint-enable sort-keys, sort-keys/sort-keys-fix, perfectionist/sort-objects */
+})
+
+export default fractalPreset
