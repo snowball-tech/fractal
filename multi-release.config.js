c89da3f6d08bf95b5b07c63d6a034ad00174b8ad
Clément PRÉVOT <clement@snowball.xyz>
2023-07-06 15:02:33 +0200

    deps(fractal): update design tokens

--- multi-release.config.js
+++ multi-release.config.js
@@ -1,8 +1,3 @@
 module.exports = {
   extends: '@snowball-tech/semantic-release-config/multi-release.config.js',
-
-  // eslint-disable-next-line sort-keys, sort-keys/sort-keys-fix, perfectionist/sort-objects
-  deps: {
-    bump: 'inherit',
-  },
 }
