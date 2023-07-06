d013a10cd7355a39091777c3e1af60cd55a8b90c
Clément PRÉVOT <clement@snowball.xyz>
2023-07-06 14:35:40 +0200

    feat(design-tokens): add `cursor` tokens

---
+++ packages/design-tokens/tokens/cursor.js
@@ -0,0 +1,22 @@
+module.exports = {
+  cursor: {
+    default: {
+      comment: 'Default cursor.',
+
+      value: 'default',
+    },
+
+    // eslint-disable-next-line sort-keys, sort-keys/sort-keys-fix, perfectionist/sort-objects
+    clickable: {
+      comment: 'Cursor for clickable elements.',
+
+      value: 'pointer',
+    },
+
+    disabled: {
+      comment: 'Cursor for disabled elements.',
+
+      value: 'not-allowed',
+    },
+  },
+}
