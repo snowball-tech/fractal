131e33c45afb5a7cd06a483fefb531b9e82f8230
Clément PRÉVOT <clement@snowball.xyz>
2023-07-10 17:19:20 +0200

    feat(button): use ArkUI pressable
    Also improve event handlers and doc

--- packages/fractal/src/components/Button/Button.tsx
+++ packages/fractal/src/components/Button/Button.tsx
@@ -1,3 +1,4 @@
+import { Pressable } from '@ark-ui/react'
 import { cx } from '@snowball-tech/fractal-panda/css'
 import { button, typography } from '@snowball-tech/fractal-panda/recipes'
 import isFunction from 'lodash/fp/isFunction'
@@ -15,6 +16,7 @@ export default function Button({
   iconPosition = 'right',
   label,
   onClick,
+  onLongClick,
   type = 'button',
   variant = DEFAULT_VARIANT,
   ...props
@@ -22,18 +24,21 @@ export default function Button({
   const buttonClassNames = cx(button({ variant }), props.className)
 
   return (
-    <button
+    <Pressable
       className={buttonClassNames}
       disabled={disabled}
       type={type}
-      {...(isFunction(onClick) ? { onClick: (event) => onClick(event) } : {})}
-      {...omit(['className'], props)}
+      {...(isFunction(onClick) ? { onPress: (event) => onClick(event) } : {})}
+      {...(isFunction(onLongClick)
+        ? { onLongPress: (event) => onLongClick(event) }
+        : {})}
+      {...omit(['className', 'dir'], props)}
     >
       {icon && iconPosition === 'left' && icon}
 
       <span className={typography({ variant: 'body-1' })}>{label}</span>
 
       {icon && iconPosition === 'right' && icon}
-    </button>
+    </Pressable>
   )
 }
--- packages/fractal/src/components/Button/Button.types.ts
+++ packages/fractal/src/components/Button/Button.types.ts
@@ -1,8 +1,12 @@
-import type { AllHTMLAttributes, MouseEvent, ReactNode } from 'react'
+import type { PressableProps } from '@ark-ui/react'
+import type { AllHTMLAttributes, ReactNode } from 'react'
 
 import { Variants } from './Button.constants'
 
-export interface ButtonProps extends AllHTMLAttributes<HTMLButtonElement> {
+export interface ButtonProps
+  extends Omit<AllHTMLAttributes<HTMLButtonElement>, 'onClick'> {
+  /** The reading direction of the button. */
+  dir?: PressableProps['dir']
   /** Prevents the user from interacting with the button. */
   disabled?: boolean
   /** The icon to display in the button (if any). */
@@ -12,7 +16,11 @@ export interface ButtonProps extends AllHTMLAttributes<HTMLButtonElement> {
   /** The label of the button. */
   label: string
   /** Event handler called when the button is clicked. */
-  onClick?: (event: MouseEvent) => void
+  onClick?: PressableProps['onPress']
+  /**
+   * Event handler called when the button is clicked for a long time (>= 500ms).
+   */
+  onLongClick?: PressableProps['onLongPress']
   /** The type of button. */
   type?: 'button' | 'reset' | 'submit'
   /** The variant of the button to use. */
