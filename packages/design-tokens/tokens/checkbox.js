1e7e06dd66a759002a489b6f12e9736624b6fddf
Clément PRÉVOT <clement@snowball.xyz>
2023-07-18 11:06:36 +0200

    fix(design-tokens): fix radio token

--- packages/design-tokens/tokens/checkbox.js
+++ packages/design-tokens/tokens/checkbox.js
@@ -79,31 +79,31 @@ module.exports = {
         },
 
         // eslint-disable-next-line sort-keys, sort-keys/sort-keys-fix, perfectionist/sort-objects
-        check: {
+        checked: {
           1: {
             comment:
               'Background color to use when the first (and 5n+1th) radio button in a group is selected',
-            value: '{color.decorative.pink.90.value}',
+            value: '{color.decorative.pink.70.value}',
           },
           2: {
             comment:
               'Background color to use when the second (and 5n+2th) radio button in a group is selected',
-            value: '{color.decorative.yellow.90.value}',
+            value: '{color.decorative.yellow.70.value}',
           },
           3: {
             comment:
               'Background color to use when the third (and 5n+3th) radio button in a group is selected',
-            value: '{color.decorative.green.90.value}',
+            value: '{color.decorative.green.70.value}',
           },
           4: {
             comment:
               'Background color to use when the fourth (and 5n+4th) radio button in a group is selected',
-            value: '{color.decorative.blue.90.value}',
+            value: '{color.decorative.blue.70.value}',
           },
           5: {
             comment:
               'Background color to use when the fifth (and 5n+5th) radio button in a group is selected',
-            value: '{color.decorative.purple.90.value}',
+            value: '{color.decorative.purple.70.value}',
           },
         },
       },
