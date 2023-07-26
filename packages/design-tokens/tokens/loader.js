fc0194ca32d40e07693797b7d4f2d7a52cf78293
Clément PRÉVOT <clement@snowball.xyz>
2023-07-26 17:20:55 +0200

    feat(design-token): add tokens for loader sizes

---
+++ packages/design-tokens/tokens/loader.js
@@ -0,0 +1,35 @@
+module.exports = {
+  size: {
+    loader: {
+      // eslint-disable-next-line sort-keys, sort-keys/sort-keys-fix, perfectionist/sort-objects
+      xs: {
+        value: '{size.spacing.3.value}',
+      },
+
+      // eslint-disable-next-line sort-keys, sort-keys/sort-keys-fix, perfectionist/sort-objects
+      s: {
+        value: '{size.spacing.4.value}',
+      },
+
+      // eslint-disable-next-line sort-keys, sort-keys/sort-keys-fix, perfectionist/sort-objects
+      m: {
+        value: '{size.spacing.5.value}',
+      },
+
+      // eslint-disable-next-line sort-keys, sort-keys/sort-keys-fix, perfectionist/sort-objects
+      l: {
+        value: '{size.spacing.6.value}',
+      },
+
+      // eslint-disable-next-line sort-keys, sort-keys/sort-keys-fix, perfectionist/sort-objects
+      xl: {
+        value: '{size.spacing.7.value}',
+      },
+
+      // eslint-disable-next-line sort-keys, sort-keys/sort-keys-fix, perfectionist/sort-objects
+      xxl: {
+        value: '{size.spacing.8.value}',
+      },
+    },
+  },
+}
