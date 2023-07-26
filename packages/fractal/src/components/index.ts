fb4807bd24da0e508c7fa542823a260ac4306464
Clément PRÉVOT <clement@snowball.xyz>
2023-07-26 17:31:50 +0200

    fix(fractal): add missing exports for new components

--- packages/fractal/src/components/index.ts
+++ packages/fractal/src/components/index.ts
@@ -7,11 +7,14 @@ export {
   AutocompleteLoading,
 } from './Autocomplete/index.js'
 export { Button } from './Button/index.js'
+export { Card } from './Card/index.js'
 export { InputCheckbox } from './InputCheckbox/index.js'
 export { InputDate } from './InputDate/index.js'
 export { InputPhone } from './InputPhone/index.js'
+export { InputPinCode } from './InputPinCode/index.js'
 export { InputRadio, InputRadioGroup } from './InputRadio/index.js'
 export { InputText } from './InputText/index.js'
+export { Loader } from './Loader/index.js'
 export {
   Select,
   SelectItem,
