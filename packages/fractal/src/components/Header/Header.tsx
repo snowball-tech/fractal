02f53b66d4a0fcb9f8ded3cc64a4a1f83d192e9e
Clément PRÉVOT <clement@snowball.xyz>
2023-08-02 14:04:11 +0200

    refactor(header): update the header component to make it composable

--- packages/fractal/src/components/Header/Header.tsx
+++ packages/fractal/src/components/Header/Header.tsx
@@ -1,52 +1,36 @@
-import { UilArrowLeft as LeftIcon } from '@iconscout/react-unicons'
 import { cx } from '@snowball-tech/fractal-panda/css'
 import {
   header,
-  headerBackButton,
-  headerMenu,
-  headerTitle,
+  headerLeft,
+  headerMiddle,
+  headerRight,
 } from '@snowball-tech/fractal-panda/recipes'
+import isFunction from 'lodash/fp/isFunction'
 import omit from 'lodash/fp/omit'
-
-import { Typography } from '@/components/Typography'
 
 import type { HeaderProps } from './Header.types'
 
 /**
- * `Header` component displays a responsive header with an integrated menu
+ * `Header` component displays a responsive header.
  */
 export const Header = ({
-  back = false,
-  children,
-  menu,
-  title,
+  children: middle,
+  left,
+  onClick,
+  right,
   ...props
 }: HeaderProps) => {
   return (
     <div
       className={cx(props.className, header())}
+      {...(isFunction(onClick) ? { onClick } : {})}
       {...omit(['className'], props)}
     >
-      {back ? (
-        <div className={headerBackButton()}>
-          <LeftIcon />
-        </div>
-      ) : (
-        false
-      )}
+      {left ? <div className={headerLeft()}>{left}</div> : false}
 
-      <Typography
-        aria-label={title}
-        className={headerTitle()}
-        title={title}
-        variant="heading-4"
-      >
-        {title}
-      </Typography>
+      {middle ? <div className={headerMiddle()}>{middle}</div> : false}
 
-      {children}
-
-      {menu ? <div className={headerMenu()}>{menu}</div> : false}
+      {right ? <div className={headerRight()}>{right}</div> : false}
     </div>
   )
 }
--- packages/fractal/src/components/Header/Header.types.ts
+++ packages/fractal/src/components/Header/Header.types.ts
@@ -1,30 +1,12 @@
 import type { AllHTMLAttributes, MouseEvent, ReactNode } from 'react'
 
 export interface HeaderProps extends AllHTMLAttributes<HTMLDivElement> {
-  /** The avatar to display on the right of the header in wide mode. */
-  avatar?: ReactNode
-  /**
-   * Indicates of you want to display the back button on the left of the header
-   * in responsive mode.
-   */
-  back?: boolean
-  /** The content to display in the middle header in wide mode. */
+  /** The content to display in the middle of the header. */
   children?: ReactNode
-  /** The logo to display on the left of the header in wide mode. */
-  logo?: ReactNode
-  /**
-   * The hamburger menu to display on the right of the header in responsive
-   * mode.
-   */
-  menu?: ReactNode
-  /** The event handler called when the back button is clicked. */
-  onBack?: () => void
+  /** The content to display on the left of the header. */
+  left?: ReactNode
   /** The event handler called when the header is clicked. */
   onClick?: (event: MouseEvent<HTMLDivElement>) => void
-  /** The event handler called when the logo is clicked. */
-  onLogoClick?: (event: MouseEvent<HTMLDivElement>) => void
-  /** The event handler called when the title is clicked. */
-  onTitleClick?: (event: MouseEvent<HTMLDivElement>) => void
-  /** The title to display in the header in responsive mode. */
-  title: string
+  /** The content to display on the right of the header. */
+  right?: ReactNode
 }
