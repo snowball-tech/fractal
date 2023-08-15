df1eb88402f9c8bd5f196a720be51be56e3ea98f
Clément PRÉVOT <clement@snowball.xyz>
2023-08-16 01:43:50 +0200

    feat(avatar): add avatar design tokens

---
+++ packages/design-tokens/tokens/avatar.js
@@ -0,0 +1,23 @@
+module.exports = {
+  size: {
+    avatar: {
+      s: {
+        value: 40,
+      },
+
+      // eslint-disable-next-line sort-keys, sort-keys/sort-keys-fix, perfectionist/sort-objects
+      m: {
+        value: 48,
+      },
+
+      // eslint-disable-next-line sort-keys, sort-keys/sort-keys-fix, perfectionist/sort-objects
+      l: {
+        value: 56,
+      },
+
+      xl: {
+        value: 64,
+      },
+    },
+  },
+}
