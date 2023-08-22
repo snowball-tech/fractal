ab4afd089f810bac5385de969638201a3a901746
Clément PRÉVOT <clement@snowball.xyz>
2023-08-22 16:33:21 +0200

    feat(card): allow to dismiss a card

--- packages/fractal/src/components/Card/Card.types.ts
+++ packages/fractal/src/components/Card/Card.types.ts
@@ -7,22 +7,24 @@ export interface CardProps extends AllHTMLAttributes<HTMLDivElement> {
   children: ReactNode
   /** The background color of the card. */
   color?: `${Colors}`
+  /** Indicates if we can dismiss the card. */
+  dismissable?: boolean
   /**
    * The elevation level of the card.
    *
    * 0 (flat) is a non elevated flat card
-   * 1 (bordered) is a non elevatec bordered card
+   * 1 (bordered) is a non elevated bordered card
    * 2 (low) is a lightly raised (small shadow) bordered card
    * 3 (high) is a highly raised (big shadow) heavily bordered card
    */
   elevation?: `${Elevations}`
-  /** indicates if the card should take all the available width or not. */
-  fullWidth?: boolean
   /**
    * An icon to display at the top of the card (to the left of the title if
    * there is one).
    */
   icon?: ReactNode
+  /** Event handler called when the card is dismissed. */
+  onDismiss?: () => void
   /** A title to display at the top of the card. */
   title?: string
 }
