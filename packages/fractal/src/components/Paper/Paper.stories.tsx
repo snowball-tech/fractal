8e49a07a7c75f8045d34bbc31ab84b366b63f139
Clément PRÉVOT <clement@snowball.xyz>
2023-08-22 19:19:01 +0200

    docs(paper): fix the default elevation value

--- packages/fractal/src/components/Paper/Paper.stories.tsx
+++ packages/fractal/src/components/Paper/Paper.stories.tsx
@@ -25,6 +25,7 @@ const meta = {
   args: {
     children:
       'Size matters not. Look at me. Judge me by my size, do you? Hmm? Hmm. And well you should not. For my ally is the Force, and a powerful ally it is. Life creates it, makes it grow. Its energy surrounds us and binds us. Luminous beings are we, not this crude matter. You must feel the Force around you; here, between you, me, the tree, the rock, everywhere, yes. Even between the land and the ship.',
+    elevation: DEFAULT_ELEVATION,
   },
   component: Paper,
   parameters: {
