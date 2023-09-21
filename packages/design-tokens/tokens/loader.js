3b901bbfa0cdffa58e6cde2edafb0b382a5d4274
Clément PRÉVOT <clement@snowball.xyz>
2023-09-21 11:10:08 +0200

    style(loader): update the available loader sizes

--- packages/design-tokens/tokens/loader.js
+++ packages/design-tokens/tokens/loader.js
@@ -1,18 +1,8 @@
 module.exports = {
   size: {
     loader: {
-      xs: {
-        value: '{size.spacing.3.value}',
-      },
-
-      // eslint-disable-next-line sort-keys, sort-keys/sort-keys-fix, perfectionist/sort-objects
-      s: {
-        value: '{size.spacing.4.value}',
-      },
-
-      // eslint-disable-next-line sort-keys, sort-keys/sort-keys-fix, perfectionist/sort-objects
       m: {
-        value: '{size.spacing.5.value}',
+        value: '{size.spacing.4.value}',
       },
 
       // eslint-disable-next-line sort-keys, sort-keys/sort-keys-fix, perfectionist/sort-objects
@@ -22,12 +12,7 @@ module.exports = {
 
       // eslint-disable-next-line sort-keys, sort-keys/sort-keys-fix, perfectionist/sort-objects
       xl: {
-        value: '{size.spacing.7.value}',
-      },
-
-      // eslint-disable-next-line sort-keys, sort-keys/sort-keys-fix, perfectionist/sort-objects
-      xxl: {
-        value: '{size.spacing.8.value}',
+        value: '{size.spacing.20.value}',
       },
     },
   },
