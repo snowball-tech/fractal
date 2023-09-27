e47ad631b0ebacd25166e78184f00f359dcaf923
Clément PRÉVOT <clement@snowball.xyz>
2023-09-27 13:20:29 +0200

    deps: upgrade dependencies

--- packages/fractal/src/components/Autocomplete/AutocompleteLoading.stories.tsx
+++ packages/fractal/src/components/Autocomplete/AutocompleteLoading.stories.tsx
@@ -42,7 +42,10 @@ type Story = StoryObj<typeof meta>
 
 export const Playground: Story = {
   render: ({ children, icon, spin = false }) => (
-    <Autocomplete placeholder="Start typing to autocomplete">
+    <Autocomplete
+      placeholder="Start typing to autocomplete"
+      style={{ minWidth: '500px' }}
+    >
       <AutocompleteLoading icon={icon} spin={spin}>
         {children}
       </AutocompleteLoading>
