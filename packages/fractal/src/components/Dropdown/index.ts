201b1b7346c28cda6104d1290b9e432904812079
Clément PRÉVOT <clement@snowball.xyz>
2023-10-10 20:31:10 +0200

    fix(dropdown): add missing exports

--- packages/fractal/src/components/Dropdown/index.ts
+++ packages/fractal/src/components/Dropdown/index.ts
@@ -4,6 +4,11 @@ export type {
   DropdownItemProps,
   DropdownItemSeparatorProps,
   DropdownProps,
+  DropdownRadioGroupProps,
+  DropdownRadioItemProps,
 } from './Dropdown.types.js'
 export { default as DropdownItem } from './DropdownItem.js'
+export { default as DropdownItemGroup } from './DropdownItemGroup.js'
 export { default as DropdownItemSeparator } from './DropdownItemSeparator.js'
+export { default as DropdownRadioGroup } from './DropdownRadioGroup.js'
+export { default as DropdownRadioItem } from './DropdownRadioItem.js'
