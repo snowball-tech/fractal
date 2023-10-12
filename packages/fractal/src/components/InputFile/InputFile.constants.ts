950ae4a15a969646e628cdfbde90f0ad9bcf441c
Clément PRÉVOT <clement@snowball.xyz>
2023-10-12 10:51:27 +0200

    feat(input-file): add an input file component
    It can only be triggered by a button

---
+++ packages/fractal/src/components/InputFile/InputFile.constants.ts
@@ -0,0 +1,8 @@
+export enum Variants {
+  Display = 'display',
+  Primary = 'primary',
+  Secondary = 'secondary',
+  Text = 'text',
+}
+
+export const DEFAULT_VARIANT = Variants.Primary
---
+++ packages/fractal/src/components/InputFile/InputFile.types.ts
@@ -0,0 +1,27 @@
+import type { AllHTMLAttributes } from 'react'
+
+import { Variants } from './InputFile.constants'
+
+export type CombinedRefs = {
+  fileInput: HTMLInputElement | null
+  trigger: HTMLButtonElement | null
+}
+
+export interface InputFileProps
+  extends Omit<AllHTMLAttributes<HTMLInputElement>, 'onChange'> {
+  /** Prevents the user from interacting with the input file and the trigger. */
+  disabled?: boolean
+  /** The label of the trigger of the input file. */
+  label: string
+  /** Event handler called when one or multiple files are selected. */
+  onChange?: (files: FileList | null) => void
+  /** The props to pass to the trigger of the input file. */
+  triggerProps?: Record<string, unknown>
+  /**
+   * The variant of the input file.
+   *
+   * Currently, only trigger button variants are available (and the variants
+   * name follow the variants name of the `Button` component).
+   */
+  variant?: `${Variants}`
+}
