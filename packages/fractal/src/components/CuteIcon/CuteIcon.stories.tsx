47408d3e7f226da6a555cd7cf201b9b52a0b481f
Clément PRÉVOT <clement@snowball.xyz>
2023-12-07 16:43:46 +0000

    feat(cute-icon): add the cute icon component

---
+++ packages/fractal/src/components/CuteIcon/CuteIcon.stories.tsx
@@ -0,0 +1,77 @@
+import CancelIcon from '@iconscout/react-unicons/dist/icons/uil-cancel'
+import CheckCircleIcon from '@iconscout/react-unicons/dist/icons/uil-check-circle'
+import StarIcon from '@iconscout/react-unicons/dist/icons/uil-envelope-star'
+import ExclamationCircleIcon from '@iconscout/react-unicons/dist/icons/uil-exclamation-circle'
+import SendIcon from '@iconscout/react-unicons/dist/icons/uil-message'
+import SearchIcon from '@iconscout/react-unicons/dist/icons/uil-search-alt'
+import type { Meta, StoryObj } from '@storybook/react'
+import type { ComponentProps } from 'react'
+
+import { CuteIcon, CuteIconColors } from '.'
+import { DEFAULT_COLOR } from './CuteIcon.constants'
+
+type CuteIconProps = ComponentProps<typeof CuteIcon>
+
+const meta = {
+  argTypes: {
+    color: {
+      options: Object.values(CuteIconColors),
+      table: {
+        defaultValue: { summary: DEFAULT_COLOR },
+        type: { summary: Object.values(CuteIconColors).join('|') },
+      },
+    },
+    icon: {
+      mapping: {
+        Cancel: <CancelIcon />,
+        Check: <CheckCircleIcon />,
+        Error: <ExclamationCircleIcon />,
+        Search: <SearchIcon />,
+        Send: <SendIcon />,
+        Star: <StarIcon />,
+      },
+      options: ['Cancel', 'Check', 'Error', 'Send', 'Star'],
+    },
+  },
+  args: {
+    icon: 'Star',
+  },
+  component: CuteIcon,
+  parameters: {
+    componentSubtitle: "⛄ Who's my cute little reindeer? - Olaf - Frozen",
+  },
+
+  title: 'Molecules/CuteIcon',
+} satisfies Meta<CuteIconProps>
+
+export default meta
+type Story = StoryObj<typeof meta>
+
+export const Playground: Story = {
+  args: {
+    color: DEFAULT_COLOR,
+  },
+}
+
+export const CuteIcons: Story = {
+  parameters: {
+    controls: {
+      include: ['icon'],
+    },
+  },
+  render: ({ icon }) => (
+    <div
+      style={{
+        display: 'flex',
+        flexDirection: 'column',
+        gap: 'var(--size-spacing-3)',
+      }}
+    >
+      <CuteIcon color="blue" icon={icon} />
+      <CuteIcon color="yellow" icon={icon} />
+      <CuteIcon color="pink" icon={icon} />
+      <CuteIcon color="green" icon={icon} />
+      <CuteIcon color="purple" icon={icon} />
+    </div>
+  ),
+}
---
+++ packages/fractal/src/components/CuteIcon/index.ts
@@ -0,0 +1,3 @@
+export { Colors as CuteIconColors } from './CuteIcon.constants.js'
+export { default as CuteIcon } from './CuteIcon.js'
+export type { CuteIconProps } from './CuteIcon.types.js'
