df14550e8a7aaf4893fbd48d947c725766ee7f83
Clément PRÉVOT <clement@snowball.xyz>
2023-12-20 16:35:19 +0000

    fix(input-file): update the typo of the button

--- packages/fractal/src/components/Card/Card.types.ts
+++ packages/fractal/src/components/Card/Card.types.ts
@@ -16,7 +16,7 @@ export interface CardProps extends AllHTMLAttributes<HTMLDivElement> {
   dismissable?: boolean
   /**
    * Indicate the font size of the title and the body of the card.
-   * 1 is `body-1` adn 2 is `body-2`.
+   * 1 is `body-1` and 2 is `body-2`.
    */
   fontSize?: 1 | 2
   /**
