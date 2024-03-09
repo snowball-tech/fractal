771ff1fc6aa7b17e63db7f02049e1743809af27c
Clément PRÉVOT <clement@snowball.xyz>
2024-03-09 23:04:00 +0000

    fix(typography): allow to have empty children
    In the case where we want to use a Typography as a content editable

--- packages/fractal/src/components/Typography/Typography.types.ts
+++ packages/fractal/src/components/Typography/Typography.types.ts
@@ -4,7 +4,7 @@ import { Variants } from './Typography.constants'
 
 export interface TypographyProps extends AllHTMLAttributes<HTMLElement> {
   /** The text to display. */
-  children: ReactNode
+  children?: ReactNode
   /** The HTML element to use to display your text. */
   element?: ElementType
   /** The variant of typography (style and size) to use. */
