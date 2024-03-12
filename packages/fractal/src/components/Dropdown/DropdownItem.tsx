a956cc7564d485fadd438cd6995dad72dba0248e
Clément PRÉVOT <clement@snowball.xyz>
2024-03-12 11:50:44 +0000

    fix(dropdown-item): add forward ref

--- packages/fractal/src/components/Dropdown/DropdownItem.tsx
+++ packages/fractal/src/components/Dropdown/DropdownItem.tsx
@@ -2,7 +2,7 @@ import * as RxDropdownMenu from '@radix-ui/react-dropdown-menu'
 import isEmpty from 'lodash/fp/isEmpty'
 import isFunction from 'lodash/fp/isFunction'
 import omit from 'lodash/fp/omit'
-import { useContext } from 'react'
+import { type ForwardedRef, forwardRef, useContext } from 'react'
 
 import { Typography } from '@/components/Typography/Typography'
 import { PREFIX } from '@/constants'
@@ -19,75 +19,81 @@ import { DropdownGroupContext } from './DropdownGroupContext'
  * See https://www.radix-ui.com/primitives/docs/components/dropdown-menu#item
  * for more information.
  */
-export const DropdownItem = ({
-  active = false,
-  children,
-  disabled,
-  href,
-  icon,
-  label,
-  onClick,
-  onSelect,
-  target,
-  value,
-  ...props
-}: DropdownItemProps) => {
-  const hasChildren = Boolean(children)
-  if (!hasChildren && isEmpty(label)) {
-    console.warn(
-      'You must provide a `label` or `children` to the `DropdownItem` component',
-    )
-  }
+export const DropdownItem = forwardRef<HTMLDivElement, DropdownItemProps>(
+  (
+    {
+      active = false,
+      children,
+      disabled,
+      href,
+      icon,
+      label,
+      onClick,
+      onSelect,
+      target,
+      value,
+      ...props
+    }: DropdownItemProps,
+    ref: ForwardedRef<HTMLDivElement>,
+  ) => {
+    const hasChildren = Boolean(children)
+    if (!hasChildren && isEmpty(label)) {
+      console.warn(
+        'You must provide a `label` or `children` to the `DropdownItem` component',
+      )
+    }
 
-  const { disabled: dropdownDisabled } = useContext(DropdownContext)
-  const { disabled: groupDisabled } = useContext(DropdownGroupContext)
+    const { disabled: dropdownDisabled } = useContext(DropdownContext)
+    const { disabled: groupDisabled } = useContext(DropdownGroupContext)
 
-  const isDisabled = disabled || groupDisabled || dropdownDisabled
+    const isDisabled = disabled || groupDisabled || dropdownDisabled
 
-  const isLink = !isEmpty(href)
+    const isLink = !isEmpty(href)
 
-  return (
-    <RxDropdownMenu.Item
-      className={cn(
-        `${PREFIX}-${GROUP_NAME}__item`,
-        'alternatee',
-        'flex items-center gap-1',
-        'rounded-sm p-2 outline-none transition-background-color duration-300 ease-out',
-        icon ? `${PREFIX}-${GROUP_NAME}__with-icon` : '',
-        isDisabled
-          ? `${PREFIX}-${GROUP_NAME}__item--disabled cursor-not-allowed !bg-transparent text-disabled`
-          : 'cursor-pointer text-dark',
-        isLink ? `${PREFIX}-${GROUP_NAME}__item__link no-underline` : '',
-        props.className,
-      )}
-      {...(active ? { 'data-highlighted': active } : {})}
-      {...(isLink ? { href, target } : {})}
-      {...(value !== undefined ? { 'data-value': value } : {})}
-      disabled={isDisabled}
-      {...(isFunction(onClick) ? { onSelect: onClick } : {})}
-      {...(isFunction(onSelect) ? { onSelect } : {})}
-      {...omit(['className', 'data-value'], props)}
-      aria-label={label}
-      asChild
-      title={label}
-    >
-      <Typography element={isLink ? 'a' : 'div'}>
-        {icon && (
-          <div
-            className={cj(
-              `${PREFIX}-${GROUP_NAME}__item__icon`,
-              'max-h-3 max-w-3',
-            )}
-          >
-            {icon}
-          </div>
+    return (
+      <RxDropdownMenu.Item
+        ref={ref}
+        className={cn(
+          `${PREFIX}-${GROUP_NAME}__item`,
+          'alternatee',
+          'flex items-center gap-1',
+          'rounded-sm p-2 outline-none transition-background-color duration-300 ease-out',
+          icon ? `${PREFIX}-${GROUP_NAME}__with-icon` : '',
+          isDisabled
+            ? `${PREFIX}-${GROUP_NAME}__item--disabled cursor-not-allowed !bg-transparent text-disabled`
+            : 'cursor-pointer text-dark',
+          isLink ? `${PREFIX}-${GROUP_NAME}__item__link no-underline` : '',
+          props.className,
         )}
+        {...(active ? { 'data-highlighted': active } : {})}
+        {...(isLink ? { href, target } : {})}
+        {...(value !== undefined ? { 'data-value': value } : {})}
+        disabled={isDisabled}
+        {...(isFunction(onClick) ? { onSelect: onClick } : {})}
+        {...(isFunction(onSelect) ? { onSelect } : {})}
+        {...omit(['className', 'data-value'], props)}
+        aria-label={label}
+        asChild
+        title={label}
+      >
+        <Typography element={isLink ? 'a' : 'div'}>
+          {icon && (
+            <div
+              className={cj(
+                `${PREFIX}-${GROUP_NAME}__item__icon`,
+                'max-h-3 max-w-3',
+              )}
+            >
+              {icon}
+            </div>
+          )}
 
-        {hasChildren ? children : label}
-      </Typography>
-    </RxDropdownMenu.Item>
-  )
-}
+          {hasChildren ? children : label}
+        </Typography>
+      </RxDropdownMenu.Item>
+    )
+  },
+)
 DropdownItem.displayName = 'DropdownItem'
 
 export default DropdownItem
