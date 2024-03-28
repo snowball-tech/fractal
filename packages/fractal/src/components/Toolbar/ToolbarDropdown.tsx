6d5a4cdd6ccb6d741f44c26305353e822fad954e
Clément PRÉVOT <clement@snowball.xyz>
2024-03-28 18:15:20 +0000

    fix(dropdown-popover): allow to unwrap trigger from `button`

--- packages/fractal/src/components/Toolbar/ToolbarDropdown.tsx
+++ packages/fractal/src/components/Toolbar/ToolbarDropdown.tsx
@@ -7,12 +7,12 @@ import { type ForwardedRef, forwardRef, useContext, useState } from 'react'
 
 import { Dropdown, DropdownElevations } from '@/components/Dropdown'
 import { CombinedRefs as DropdownCombinedRefs } from '@/components/Dropdown/Dropdown.types'
-import { Typography } from '@/components/Typography/Typography'
 import { PREFIX } from '@/constants'
-import { cj, cn } from '@/styles/helpers'
+import { cn } from '@/styles/helpers'
 
 import { GROUP_NAME } from './Toolbar.constants'
 import type { ToolbarDropdownProps } from './Toolbar.types'
+import ToolbarButton from './ToolbarButton'
 import { ToolbarContext } from './ToolbarContext'
 
 /**
@@ -37,7 +37,6 @@ export const ToolbarDropdown = forwardRef<
     }: ToolbarDropdownProps,
     ref: ForwardedRef<DropdownCombinedRefs>,
   ) => {
-    const hasIcon = Boolean(icon)
     if (isEmpty(label)) {
       console.warn(
         'You must provide a `label` to the `ToolbarDropdown` component',
@@ -48,7 +47,8 @@ export const ToolbarDropdown = forwardRef<
 
     const isDisabled = disabled || toolbarDisabled
 
-    const [isOpen, setIsOpen] = useState(props.defaultOpen || props.open)
+    const [isOpen, setIsOpen] = useState(false)
+
     const handleMenuOpenChange = (open: boolean) => {
       if (isFunction(props.onMenuOpenChange)) {
         props.onMenuOpenChange(open)
@@ -82,63 +82,28 @@ export const ToolbarDropdown = forwardRef<
             'data-[side="bottom"]:mt-2 data-[side="top"]:mb-2 data-[side="right"]:ml-[calc(theme(spacing.2)+theme(spacing.half))] data-[side="left"]:mr-[calc(theme(spacing.2)+theme(spacing.half))]',
         }}
         elevation={elevation}
+        open={props.open || isOpen}
         title={label}
         trigger={
-          <div
-            className={cn(
-              `${PREFIX}-${GROUP_NAME}__dropdown__trigger`,
-              'flex h-3 max-h-3 max-w-full items-center justify-center gap-1 rounded-xs bg-transparent p-0 text-left outline-none transition-colors duration-300 ease-out active:transition-none',
-              isDisabled
-                ? `${PREFIX}-${GROUP_NAME}__dropdown__trigger--disabled cursor-not-allowed`
-                : `cursor-pointer hover:text-dark group-hover:text-dark`,
-              // eslint-disable-next-line no-nested-ternary
-              (active || isOpen) && !isDisabled
-                ? 'text-dark'
-                : isDisabled
-                  ? 'text-disabled'
-                  : 'text-placeholder',
-              !isEmpty(icon)
-                ? `${PREFIX}-${GROUP_NAME}__dropdown__trigger--addendum ${PREFIX}-${GROUP_NAME}--addendum--${iconPosition === 'left' ? 'prefix' : 'suffix'}`
-                : '',
-              iconOnly
-                ? `${PREFIX}-${GROUP_NAME}__dropdown__trigger--icon-only px-half`
-                : 'w-fit',
-              // eslint-disable-next-line no-nested-ternary
-              !iconOnly
-                ? // eslint-disable-next-line no-nested-ternary
-                  hasIcon
-                  ? iconPosition === 'left'
-                    ? 'pl-half pr-1'
-                    : 'pl-1 pr-half'
-                  : 'px-1'
-                : '',
-              props.className,
-            )}
-          >
-            {hasIcon && iconPosition === 'left' && icon}
-
-            {!iconOnly && (
-              <Typography
-                className={cj(
-                  `${PREFIX}-${GROUP_NAME}__toggle__label`,
-                  'max-h-full max-w-full flex-1 overflow-hidden text-ellipsis whitespace-nowrap text-center align-middle',
-                  isDisabled
-                    ? `${PREFIX}-${GROUP_NAME}__toggle_label--disabled cursor-not-allowed`
-                    : `cursor-pointer`,
-                )}
-                element="label"
-                variant={active ? 'body-1-median' : 'body-1'}
-              >
-                {label}
-              </Typography>
-            )}
-
-            {hasIcon && iconPosition === 'right' && icon}
-          </div>
+          <ToolbarButton
+            active={active}
+            disabled={isDisabled}
+            icon={icon}
+            iconOnly={iconOnly}
+            iconPosition={iconPosition}
+            label={label}
+          />
         }
+        triggerAsButton={false}
         onMenuOpenChange={handleMenuOpenChange}
         {...omit(
-          ['className', 'condensed', 'trigger', 'onMenuOpenChange'],
+          [
+            'className',
+            'condensed',
+            'trigger',
+            'onMenuOpenChange',
+            'triggerAsButton',
+          ],
           props,
         )}
       >
