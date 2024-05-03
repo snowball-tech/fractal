2ac6d9191831aade2883b98d3784f946297fc365
Clément PRÉVOT <clement@snowball.xyz>
2024-05-03 15:51:26 +0000

    feat(typography): add an `inlineStyle` props
    This allow to inline the style of the typography instead of using classes
    Note that classes are still output though

--- packages/fractal/src/components/Typography/Typography.types.ts
+++ packages/fractal/src/components/Typography/Typography.types.ts
@@ -7,6 +7,12 @@ export interface TypographyProps extends AllHTMLAttributes<HTMLElement> {
   children?: ReactNode
   /** The HTML element to use to display your text. */
   element?: ElementType
+  /**
+   * Indicates to inline the styles instead of using Tailwind CSS classes.
+   *
+   * The typical usage for this is when creating HTML for an email.
+   */
+  inlineStyle?: boolean
   /** The variant of typography (style and size) to use. */
   variant?: `${Variants}`
 }
