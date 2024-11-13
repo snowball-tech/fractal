5734cd3e06ff4bedd810343650232b741122a00f
Clément PRÉVOT <clement@snowball.xyz>
2024-11-13 11:46:08 +0100

    deps: update eslint config

--- packages/design-tokens/eslint.config.mjs
+++ packages/design-tokens/eslint.config.mjs
@@ -1,31 +1,10 @@
 /* eslint-disable import/extensions */
 
-import base from '@snowball-tech/eslint-snowball-config/configs/base.js'
-import html from '@snowball-tech/eslint-snowball-config/configs/html.js'
-import importConfig from '@snowball-tech/eslint-snowball-config/configs/import.js'
-import json from '@snowball-tech/eslint-snowball-config/configs/json.js'
-import lodash from '@snowball-tech/eslint-snowball-config/configs/lodash.js'
-import markdown from '@snowball-tech/eslint-snowball-config/configs/markdown.js'
-import perfectionist from '@snowball-tech/eslint-snowball-config/configs/perfectionist.js'
-import prettier from '@snowball-tech/eslint-snowball-config/configs/prettier.js'
-import secrets from '@snowball-tech/eslint-snowball-config/configs/secrets.js'
-import tailwind from '@snowball-tech/eslint-snowball-config/configs/tailwind.js'
-import typescript from '@snowball-tech/eslint-snowball-config/configs/typescript.js'
-import yml from '@snowball-tech/eslint-snowball-config/configs/yml.js'
+// eslint-disable-next-line import/no-relative-packages
+import defaultConfig from '../../eslint.config.mjs'
 
 export default [
-  ...base,
-  ...html,
-  ...json,
-  ...markdown,
-  ...yml,
-  ...secrets,
-  ...importConfig,
-  ...typescript,
-  ...lodash,
-  ...perfectionist,
-  ...tailwind,
-  ...prettier,
+  ...defaultConfig,
 
   {
     files: ['src/tailwind.config.ts'],
