a80e531330ab70a5330e885f0b94ca379b2d4450
Clément PRÉVOT <clement@snowball.xyz>
2025-07-09 17:39:35 +0200

    chore: disabled HTML linting due to an unexpected error
    I don't have time to fix that just yet

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,5 +1,5 @@
 import base from '@snowball-tech/eslint-snowball-config/configs/base.js'
-import html from '@snowball-tech/eslint-snowball-config/configs/html.js'
+// import html from '@snowball-tech/eslint-snowball-config/configs/html.js'
 import importConfig from '@snowball-tech/eslint-snowball-config/configs/import.js'
 import json from '@snowball-tech/eslint-snowball-config/configs/json.js'
 import lodash from '@snowball-tech/eslint-snowball-config/configs/lodash.js'
@@ -14,7 +14,7 @@ import yml from '@snowball-tech/eslint-snowball-config/configs/yml.js'
 
 export default [
   ...base,
-  ...html,
+  // ...html,
   ...json,
   ...markdown,
   ...yml,
